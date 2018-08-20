c35e08ecd15db13cf85b85532367bd78f2ea2219
Claus Juhl Knudsen <earkivar@gmail.com>
2018-08-20 10:23:05 +0200

    add decompress-commmand, alpha

--- almanak/scripts/cli.py
+++ almanak/scripts/cli.py
@@ -2,25 +2,50 @@ import click
 
 @click.group(name='alnamak')
 def cli():
-    '''Almanak CLI. Tools, services and workflows.'''
+    '''
+    Almanak CLI. Tools, services and workflows.
+    '''
     pass
 
-@click.command('compress', short_help='init the repo')
+
+@click.command('compress', short_help='zip-compress file or folder')
 @click.option('--target', type=click.Path(writable=True, resolve_path=True),
-            help='')
-@click.option('--overwrite', is_flag=True)
+    help='specify alternate output-filepath')
+@click.option('--overwrite', is_flag=True, help='overwrite any existing file')
 @click.argument('path', type=click.Path(exists=True, resolve_path=True))
 def compress(path, target, overwrite):
-    """Generates a Zip64-compressed copy of PATH into parent-directory or TARGET"""
+    """
+    Saves a Zip64-compressed copy of PATH in its parent-directory or TARGET.
+    Use OVERWRITE to overwrite any existing file with same filename.
+    """
     click.echo('path: ' + path)
+    click.echo('target: ' + target)
+    click.echo('overwrite: ' + overwrite)
 
-@click.command()
-def clear():
+
+@click.command('decompress', short_help='decompress zipfile')
+@click.option('--target', type=click.Path(writable=True, resolve_path=True),
+    help='specify alternate output-filepath')
+@click.option('--overwrite', is_flag=True,
+    help='overwrite any existing file or directory')
+@click.argument('path', type=click.Path(exists=True, resolve_path=True))
+def decompress(path, target, overwrite):
+    """
+    Decompresses a zipfile PATH into its parent-directory or TARGET.
+    Use OVERWRITE to overwrite any existing file or directory with same name.
+    """
+    click.echo('path: ' + path)
+    click.echo('target: ' + target)
+    click.echo('overwrite: ' + overwrite)
+
+
+@click.command('help', short_help='show this help-message and exit')
+def help():
     """Clears the visible screen in a platform-agnostic way"""
     click.clear()
 
 cli.add_command(compress)
-cli.add_command(clear)
+cli.add_command(help)
 
 
 if __name__ == '__main__':
