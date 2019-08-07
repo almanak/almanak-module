5bb2f827f7c6e23a02f3139c108385556e80bbce
clausjuhl <clausjuhl@users.noreply.github.com>
2019-08-07 14:37:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='almanak',
-    version='0.2.2',
+    version='0.2.3',
     author='Claus Juhl Knudsen',
     author_email='clausjuhl@yahoo.com',
     packages=find_packages(),
@@ -14,7 +14,7 @@ setup(
     url='https://github.com/almanak/almanak',
     include_package_data=True,
     license='MIT',
-    python_requires='>=3.6.2',
+    python_requires='>=3.7.3',
     install_requires=[
         'boto3',
         'google-cloud-datastore',
@@ -22,7 +22,7 @@ setup(
     ],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
